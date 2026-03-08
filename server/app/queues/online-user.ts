63e9fa6eab5eeb1ad629a6287e88986d03bf5a32
wangruijin <1361572192@qq.com>
2026-03-08 21:11:14 +0800

    ✨ feat: 完善代码

--- server/app/queues/online-user.ts
+++ server/app/queues/online-user.ts
@@ -1,5 +1,6 @@
 import { EntityManager } from '@mikro-orm/core'
 import { Processor, WorkerHost } from '@nestjs/bullmq'
+import { Logger } from '@nestjs/common'
 import { Job } from 'bullmq'
 import { SysOnlineEntity } from '../entities'
 import { ParseService } from '../shared'
@@ -7,6 +8,8 @@ import { QueueNameEnum } from './constant'
 
 @Processor(QueueNameEnum.ONLINE_USER)
 export class OnlineUserQueue extends WorkerHost {
+  private logger = new Logger(OnlineUserQueue.name)
+
   constructor(
     private parseService: ParseService,
     private em: EntityManager,
@@ -19,18 +22,24 @@ export class OnlineUserQueue extends WorkerHost {
 
     const usResult = this.parseService.parseUserAgent(userAgent)
     const ipResult = await this.parseService.parseIP(ip)
+    const em = this.em.fork()
 
-    const online = this.em.create(SysOnlineEntity, {
-      tokenId,
-      token,
-      browser: [usResult.browser.name, usResult.browser.version].join(' '),
-      os: [usResult.os.name, usResult.os.version].join(' '),
-      ip: ipResult.ip,
-      location: ipResult.location,
-      loginTime: new Date(),
-      user,
-    })
+    try {
+      const online = em.create(SysOnlineEntity, {
+        tokenId,
+        token,
+        browser: [usResult.browser.name, usResult.browser.version].join(' '),
+        os: [usResult.os.name, usResult.os.version].join(' '),
+        ip: ipResult.ip,
+        location: ipResult.location,
+        loginTime: new Date(),
+      })
+      online.user = user
 
-    await this.em.persist(online).flush()
+      await em.persist(online).flush()
+    }
+    catch (error) {
+      this.logger.error(error)
+    }
   }
 }
