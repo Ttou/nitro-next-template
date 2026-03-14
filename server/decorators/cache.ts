e1f2c544a76813eae52d5f20dd82471d0ad19b25
wangruijin <1361572192@qq.com>
2026-03-14 14:55:06 +0800

    ✨ feat: 完善代码

--- server/decorators/cache.ts
+++ server/decorators/cache.ts
@@ -4,6 +4,10 @@ import { Reflector } from '@nestjs/core'
 
 type CacheFunc = (req: IRequest) => string
 
-export const CacheKey = Reflector.createDecorator<CacheFunc>()
+export const CacheKey = Reflector.createDecorator<CacheFunc>({
+  key: 'cache:key',
+})
 
-export const CacheTTL = Reflector.createDecorator<StringValue>()
+export const CacheTTL = Reflector.createDecorator<StringValue>({
+  key: 'cache:ttl',
+})
