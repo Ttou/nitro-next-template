9768377c0c69542e03b6c4b4e510315b5dfd82d3
wangruijin <1361572192@qq.com>
2026-03-14 11:23:49 +0800

    ✨ feat: 完善代码

--- alova.config.ts
+++ alova.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from '@alova/wormhole'
-import { camelCase } from 'es-toolkit'
 
 export default defineConfig({
   generator: [
     {
       input: 'http://localhost:3000/openapi-json',
-      output: 'src/apis',
+      output: 'src/api',
       handleApi: (apiDescriptor) => {
+        const [controllerName, handlerName] = apiDescriptor.operationId!.split('_')
+        apiDescriptor.tags = [controllerName!.replace('Controller', '')]
+        apiDescriptor.operationId = handlerName
+
         return apiDescriptor
       },
     },
